2ffaf61389845d84a85a619339b0016d54b3d8b0
taraokelly <taraokelly.dev@gmail.com>
2018-03-13 19:50:05 +0000

    Q8 - comment cleanup

--- CA/Q.08-Chse.rkt
+++ CA/Q.08-Chse.rkt
@@ -19,8 +19,8 @@ the positions where x is 1 and the elements of z otherwise. For example:
 
 #| 
 Again we don't have to worry about non-finite sized lists and binary. With a map iterating 
-over the three maps with a lambda expression, checks if the nth number x is equal to 1, if 
-it is return the the nth number z, and if not the nth number y.
+over the three maps (x, y and z) with a lambda expression, checks if the nth number x is 
+equal to 1, if it is return the the nth number of z, and if not the nth number y.
 |#
 
 (define (chse x y z)
